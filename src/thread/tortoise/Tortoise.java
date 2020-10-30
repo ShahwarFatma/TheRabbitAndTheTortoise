391bfd7969d912accfef753e9f6758abdfbdef7b
ShahwarFatma <shahwar.fatma_cs19@gla.ac.in>
2020-10-30 11:23:59 +0530

    Add fields Add a constructor and a getter method

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -1,4 +1,16 @@
 package thread.tortoise;
 
-public class Tortoise {
+public class Tortoise extends Thread {
+    private static final int MILESTONES = 5;
+
+    private final Thread tortoise;
+
+    public Tortoise() {
+        this.tortoise = new Thread(this, "Tortoise");
+    }
+
+    public Thread getTortoise() {
+        return tortoise;
+    }
+
 }

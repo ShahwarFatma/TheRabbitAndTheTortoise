03285596a2ace695a52acc425337173a9c688200
ShahwarFatma <shahwar.fatma_cs19@gla.ac.in>
2020-10-30 12:13:31 +0530

    Add switch case to Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -13,4 +13,36 @@ public class Tortoise extends Thread {
         return tortoise;
     }
 
+    public void run() {
+        for (int index = 0; index < MILESTONES; index++) {
+            switch (index) {
+                case 0:
+                    System.out.println("The Tortoise has started from the start line!");
+                    break;
+                case 1:
+                    System.out.println("The Tortoise has reached the Cool River!");
+                    break;
+                case 2:
+                    System.out.println("The Tortoise has reached the Mountain Hill!");
+                    break;
+                case 3:
+                    System.out.println("The Tortoise has reached the Big Oak Tree!");
+                    System.out.println("The Tortoise decides to go to sleep!");
+                    try {
+                        Thread.sleep(3000L);
+                    } catch (InterruptedException e) {
+                        System.err.println("The Tortoise's sleep got interrupted!");
+                    }
+                    break;
+                case 4:
+                    System.out.println("The Tortoise has reached the finish line!");
+                    break;
+            }
+            try {
+                Thread.sleep(3000L);
+            } catch (InterruptedException e) {
+                System.err.println("The tortoise has lost the path!");
+            }
+        }
+    }
 }
